cba8c33afa324f6809847af852215d5e78de9f0f
isveny <amineprojet7@gmail.com>
2021-11-13 19:08:40 +0100

    feat: shop fully functional.

--- libs/types.ts
+++ libs/types.ts
@@ -77,39 +77,243 @@ export type Shop = {
             }
           ];
         };
+        items: [
+          {
+            id: number;
+            name: string;
+            description: string;
+            type: { value: string; displayValue: string; backendValue: string };
+            rarity: {
+              value: string;
+              displayValue: string;
+              backendValue: string;
+            };
+            series: string;
+            set: { value: string; text: string; backendValue: string };
+            introduction: {
+              chapter: string;
+              season: string;
+              text: string;
+              backendValue: number;
+            };
+            images: {
+              smallIcon: string;
+              icon: string;
+              featured: string;
+              other: string;
+            };
+            variants: string;
+            searchTags: [];
+            metaTags: string;
+            showcaseVideo: string;
+            dynamicPakId: string;
+            displayAssetPath: string;
+            definitionPath: string;
+            path: string;
+            added: string;
+            shopHistory: [];
+          }
+        ];
       }
     ];
-    items: [
+  };
+  daily: {
+    name: string;
+    entries: [
       {
-        id: number;
-        name: string;
-        description: string;
-        type: { value: string; displayValue: string; backendValue: string };
-        rarity: { value: string; displayValue: string; backendValue: string };
-        series: string;
-        set: { value: string; text: string; backendValue: string };
-        introduction: {
-          chapter: string;
-          season: string;
-          text: string;
-          backendValue: number;
+        regularPrice: number;
+        finalPrice: number;
+        bundle: { name: string; info: string; image: string };
+        banner: { value: string; intensity: string; backendValue: string };
+        giftable: boolean;
+        refundable: boolean;
+        sortPriority: number;
+        categories: [];
+        sectionId: string;
+        section: {
+          id: string;
+          name: string;
+          index: number;
+          landingPriority: number;
+          sortOffersByOwnership: boolean;
+          showIneligibleOffers: boolean;
+          showIneligibleOffersIfGiftable: boolean;
+          showTimer: boolean;
+          enableToastNotification: boolean;
+          hidden: boolean;
         };
-        images: {
-          smallIcon: string;
-          icon: string;
-          featured: string;
-          other: string;
-        };
-        variants: string;
-        searchTags: [];
-        metaTags: string;
-        showcaseVideo: string;
-        dynamicPakId: string;
+        devName: string;
+        offerId: string;
         displayAssetPath: string;
-        definitionPath: string;
-        path: string;
-        added: string;
-        shopHistory: [];
+        titleSize: string;
+        newDisplayAssetPath: string;
+        newDisplayAsset: {
+          id: string;
+          cosmeticId: string;
+          materialInstances: [
+            {
+              id: string;
+              image: { OfferImage: string; Background: string };
+              colors: {
+                Background_Color_A: string;
+                Background_Color_B: string;
+                FallOff_Color: string;
+              };
+              scalings: {
+                Spotlight_Hardness: number;
+                OffsetImage_X: number;
+                OffsetImage_Y: number;
+                ZoomImage_Percent: number;
+                RefractionDepthBias: number;
+                Gradient_Position_X: number;
+                Gradient_Position_Y: number;
+                Gradient_Size: number;
+                FallOffColor_Fill_Percent: number;
+                FallOffColor_Postion: number;
+              };
+              flags: {
+                IsAnimatedWrap: boolean;
+              };
+            }
+          ];
+        };
+        items: [
+          {
+            id: number;
+            name: string;
+            description: string;
+            type: { value: string; displayValue: string; backendValue: string };
+            rarity: {
+              value: string;
+              displayValue: string;
+              backendValue: string;
+            };
+            series: string;
+            set: { value: string; text: string; backendValue: string };
+            introduction: {
+              chapter: string;
+              season: string;
+              text: string;
+              backendValue: number;
+            };
+            images: {
+              smallIcon: string;
+              icon: string;
+              featured: string;
+              other: string;
+            };
+            variants: string;
+            searchTags: [];
+            metaTags: string;
+            showcaseVideo: string;
+            dynamicPakId: string;
+            displayAssetPath: string;
+            definitionPath: string;
+            path: string;
+            added: string;
+            shopHistory: [];
+          }
+        ];
+      }
+    ];
+  };
+  specialFeatured: {
+    name: string;
+    entries: [
+      {
+        regularPrice: number;
+        finalPrice: number;
+        bundle: { name: string; info: string; image: string };
+        banner: { value: string; intensity: string; backendValue: string };
+        giftable: boolean;
+        refundable: boolean;
+        sortPriority: number;
+        categories: [];
+        sectionId: string;
+        section: {
+          id: string;
+          name: string;
+          index: number;
+          landingPriority: number;
+          sortOffersByOwnership: boolean;
+          showIneligibleOffers: boolean;
+          showIneligibleOffersIfGiftable: boolean;
+          showTimer: boolean;
+          enableToastNotification: boolean;
+          hidden: boolean;
+        };
+        devName: string;
+        offerId: string;
+        displayAssetPath: string;
+        titleSize: string;
+        newDisplayAssetPath: string;
+        newDisplayAsset: {
+          id: string;
+          cosmeticId: string;
+          materialInstances: [
+            {
+              id: string;
+              image: { OfferImage: string; Background: string };
+              colors: {
+                Background_Color_A: string;
+                Background_Color_B: string;
+                FallOff_Color: string;
+              };
+              scalings: {
+                Spotlight_Hardness: number;
+                OffsetImage_X: number;
+                OffsetImage_Y: number;
+                ZoomImage_Percent: number;
+                RefractionDepthBias: number;
+                Gradient_Position_X: number;
+                Gradient_Position_Y: number;
+                Gradient_Size: number;
+                FallOffColor_Fill_Percent: number;
+                FallOffColor_Postion: number;
+              };
+              flags: {
+                IsAnimatedWrap: boolean;
+              };
+            }
+          ];
+        };
+        items: [
+          {
+            id: number;
+            name: string;
+            description: string;
+            type: { value: string; displayValue: string; backendValue: string };
+            rarity: {
+              value: string;
+              displayValue: string;
+              backendValue: string;
+            };
+            series: string;
+            set: { value: string; text: string; backendValue: string };
+            introduction: {
+              chapter: string;
+              season: string;
+              text: string;
+              backendValue: number;
+            };
+            images: {
+              smallIcon: string;
+              icon: string;
+              featured: string;
+              other: string;
+            };
+            variants: string;
+            searchTags: [];
+            metaTags: string;
+            showcaseVideo: string;
+            dynamicPakId: string;
+            displayAssetPath: string;
+            definitionPath: string;
+            path: string;
+            added: string;
+            shopHistory: [];
+          }
+        ];
       }
     ];
   };
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,36 +9,92 @@ const Home: NextPage = () => {
   const { data } = useSWR<Shop>(`/api/shop`, fetcher);
   return (
     <>
-      <FadeIn className="flex flex-col justify-center px-8 my-36">
+      <FadeIn className="flex flex-col justify-center my-5 px-8">
         <div className="flex flex-col items-center justify-center max-w-2xl mx-auto mb-16 dark:text-white">
           <h1 className="text-5xl font-bold text-white mb-2">Shop</h1>
           <div className="flex-col justify-center items-center space-y-5">
             <div className="grid grid-cols-1 mx-5 my-2 space-y-3 mb-2">
               <div className="flex justify-center"></div>
-              <div className="w-full metric-card max-w-7xl bg-white bg-opacity-20 rounded-2xl p-4 h-80 overflow-auto pr-6">
+              <div className="w-full max-w-7xl rounded-2xl p-4 h-80 pr-6">
                 <p
                   className="mt-2 text-sm font-medium text-gray-800 spacing-sm dark:text-white"
                   data-interception="off"
                 >
                   {data ? (
                     <>
-                      <div className="flex flex-col text-gray-200">
-                        {data?.featured.items?.map((item) => {
+                      <div className="grid grid-cols-2 lg:grid-cols-4 sm:grid-cols-3 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4 w-full">
+                        {data.featured?.entries[0]?.items?.map((item) => {
                           return (
                             <>
-                              <div className="grid space-y-2">
-                                <div>
-                                  <span>name:</span>
-                                  <p>{item.name}</p>
-                                </div>
-                                <div>
-                                  <span>image:</span>
-                                  <p>{item.images.featured}</p>
+                              <div className="w-full p-4 rounded-lg bg-white bg-opacity-20 m-auto">
+                                <div className="inline-flex text-gray-200">
+                                  <div className="grid space-y-2">
+                                    <img
+                                      src={item.images.smallIcon}
+                                      className="w-full z-50 bg-cover"
+                                    />
+                                    <div>
+                                      <p>{item.name}</p>
+                                    </div>
+                                  </div>
                                 </div>
                               </div>
                             </>
                           );
                         })}
+                        {data.daily?.entries[0]?.items?.map((item) => {
+                          return (
+                            <>
+                              <div className="w-full p-4 rounded-lg bg-white bg-opacity-20">
+                                <div className="inline-flex text-gray-200">
+                                  <div className="grid space-y-2">
+                                    <img
+                                      src={item.images.smallIcon}
+                                      className="w-full z-50 bg-cover"
+                                    />
+                                    <div>
+                                      <p>{item.name}</p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </>
+                          );
+                        })}
+                        {data.specialFeatured?.entries[0]?.items?.map(
+                          (item) => {
+                            return (
+                              <>
+                                <div className="w-full p-4 rounded-lg bg-white bg-opacity-20">
+                                  <div className="inline-flex text-gray-200">
+                                    <div className="grid space-y-2">
+                                      <img
+                                        src={item.images.smallIcon}
+                                        className="w-full z-50 bg-cover"
+                                      />
+                                      <div>
+                                        <p>{item.name}</p>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </>
+                            );
+                          }
+                        )}
+                        <div className="w-full p-4 rounded-lg bg-white bg-opacity-20">
+                          <div className="inline-flex text-gray-200">
+                            <div className="grid space-y-2">
+                              <img
+                                src={data.featured?.entries[0]?.bundle.image}
+                                className="w-full z-50 bg-cover"
+                              />
+                              <div>
+                                <p>{data.featured?.entries[0]?.bundle.name}</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </>
                   ) : (
